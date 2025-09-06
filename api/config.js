f3a7e2b1c34b0c549f7ca8a99bdb39d2308ee2e0
vyza <vyzawork@gmail.com>
2025-09-06 19:28:08 +0300

    vyza

---
+++ api/config.js
@@ -0,0 +1,42 @@
+const jwt_secret = "bloxyzonesexypasswordidk";
+const coinflipwebh = "";
+const taxedItemsWebh = "";
+const botlogs = "";
+const giveawaywebh = "";
+const tippedlogs = "";
+const discordlogs = "";
+const jackpotwebh = "";
+const taxer = null; 
+const bottoken = "";
+const clientid = "1";
+const clientsecret = "whatever";
+const uri = "https://bloxyzone.com/discord/linked";
+const taxes = 0.12;  
+const xp = 0.00500;
+const crypto = {
+  bitcoin:  {
+    walletid: "btc-",
+    transferkey: "girl_JUSTNO"
+  }
+}
+
+
+
+module.exports = {
+  jwt_secret,
+  coinflipwebh,
+  taxedItemsWebh,
+  botlogs,
+  giveawaywebh,
+  taxer,
+  bottoken,
+  clientid,
+  clientsecret,
+  uri,
+  taxes,
+  crypto,
+  tippedlogs,
+  discordlogs,
+  jackpotwebh,
+  xp
+};
